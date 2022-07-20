7092f606e92f8deb6fe734313336f351e49e9917
Mohammad Shahbaz Alam <shahbaz17@users.noreply.github.com>
2022-07-20 08:31:56 +0530

    removed core

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import {useEffect, useState} from 'react'
-import {Web3AuthCore} from '@web3auth/core'
+import { useEffect, useState } from "react";
+import { Web3AuthCore } from "@web3auth/core";
 import {
   WALLET_ADAPTERS,
   CHAIN_NAMESPACES,
   SafeEventEmitterProvider,
-} from '@web3auth/base'
-import {OpenloginAdapter} from '@web3auth/openlogin-adapter'
-import './App.css'
+} from "@web3auth/base";
+import { OpenloginAdapter } from "@web3auth/openlogin-adapter";
+import "./App.css";
 // import RPC from './evm.web3'
-import RPC from './evm.ethers'
+import RPC from "./evm.ethers";
 
 const clientId =
-  'BBP_6GOu3EJGGws9yd8wY_xFT0jZIWmiLMpqrEMx36jlM61K9XRnNLnnvEtGpF-RhXJDGMJjL-I-wTi13RcBBOo' // get from https://dashboard.web3auth.io
+  "BBP_6GOu3EJGGws9yd8wY_xFT0jZIWmiLMpqrEMx36jlM61K9XRnNLnnvEtGpF-RhXJDGMJjL-I-wTi13RcBBOo"; // get from https://dashboard.web3auth.io
 
 function App() {
-  const [web3auth, setWeb3auth] = useState<Web3AuthCore | null>(null)
+  const [web3auth, setWeb3auth] = useState<Web3AuthCore | null>(null);
   const [provider, setProvider] = useState<SafeEventEmitterProvider | null>(
-    null,
-  )
+    null
+  );
 
   useEffect(() => {
     const init = async () => {
@@ -25,114 +25,114 @@ function App() {
         const web3auth = new Web3AuthCore({
           chainConfig: {
             chainNamespace: CHAIN_NAMESPACES.EIP155,
-            chainId: '0x3',
+            chainId: "0x3",
           },
-        })
+        });
 
         const openloginAdapter = new OpenloginAdapter({
           adapterSettings: {
             clientId,
-            network: 'testnet',
-            uxMode: 'popup',
+            network: "testnet",
+            uxMode: "popup",
             loginConfig: {
               google: {
-                name: 'Custom Google Auth Login',
-                verifier: 'web3auth-core-google',
-                typeOfLogin: 'google',
+                name: "Custom Google Auth Login",
+                verifier: "web3auth-core-google",
+                typeOfLogin: "google",
                 clientId:
-                  '774338308167-q463s7kpvja16l4l0kko3nb925ikds2p.apps.googleusercontent.com', //use your app client id you got from google
+                  "774338308167-q463s7kpvja16l4l0kko3nb925ikds2p.apps.googleusercontent.com", //use your app client id you got from google
               },
             },
           },
-        })
-        web3auth.configureAdapter(openloginAdapter)
-        setWeb3auth(web3auth)
+        });
+        web3auth.configureAdapter(openloginAdapter);
+        setWeb3auth(web3auth);
 
-        await web3auth.init()
+        await web3auth.init();
       } catch (error) {
-        console.error(error)
+        console.error(error);
       }
-    }
+    };
 
-    init()
-  }, [])
+    init();
+  }, []);
 
   const login = async () => {
     if (!web3auth) {
-      uiConsole('web3auth not initialized yet')
-      return
+      uiConsole("web3auth not initialized yet");
+      return;
     }
     const web3authProvider = await web3auth.connectTo(
       WALLET_ADAPTERS.OPENLOGIN,
       {
-        loginProvider: 'google',
-      },
-    )
-    setProvider(web3authProvider)
-  }
+        loginProvider: "google",
+      }
+    );
+    setProvider(web3authProvider);
+  };
 
   const getUserInfo = async () => {
     if (!web3auth) {
-      uiConsole('web3auth not initialized yet')
-      return
+      uiConsole("web3auth not initialized yet");
+      return;
     }
-    const user = await web3auth.getUserInfo()
-    uiConsole(user)
-  }
+    const user = await web3auth.getUserInfo();
+    uiConsole(user);
+  };
 
   const logout = async () => {
     if (!web3auth) {
-      uiConsole('web3auth not initialized yet')
-      return
+      uiConsole("web3auth not initialized yet");
+      return;
     }
-    await web3auth.logout()
-    setProvider(null)
-  }
+    await web3auth.logout();
+    setProvider(null);
+  };
 
   const getAccounts = async () => {
     if (!provider) {
-      uiConsole('provider not initialized yet')
-      return
+      uiConsole("provider not initialized yet");
+      return;
     }
-    const rpc = new RPC(provider)
-    const userAccount = await rpc.getAccounts()
-    uiConsole(userAccount)
-  }
+    const rpc = new RPC(provider);
+    const userAccount = await rpc.getAccounts();
+    uiConsole(userAccount);
+  };
 
   const getBalance = async () => {
     if (!provider) {
-      uiConsole('provider not initialized yet')
-      return
+      uiConsole("provider not initialized yet");
+      return;
     }
-    const rpc = new RPC(provider)
-    const balance = await rpc.getBalance()
-    uiConsole(balance)
-  }
+    const rpc = new RPC(provider);
+    const balance = await rpc.getBalance();
+    uiConsole(balance);
+  };
 
   const signMessage = async () => {
     if (!provider) {
-      uiConsole('provider not initialized yet')
-      return
+      uiConsole("provider not initialized yet");
+      return;
     }
-    const rpc = new RPC(provider)
-    const result = await rpc.signMessage()
-    uiConsole(result)
-  }
+    const rpc = new RPC(provider);
+    const result = await rpc.signMessage();
+    uiConsole(result);
+  };
 
   const sendTransaction = async () => {
     if (!provider) {
-      uiConsole('provider not initialized yet')
-      return
+      uiConsole("provider not initialized yet");
+      return;
     }
-    const rpc = new RPC(provider)
-    const result = await rpc.signAndSendTransaction()
-    uiConsole(result)
-  }
+    const rpc = new RPC(provider);
+    const result = await rpc.signAndSendTransaction();
+    uiConsole(result);
+  };
 
   function uiConsole(...args: any[]): void {
-    const el = document.querySelector('#console>p')
+    const el = document.querySelector("#console>p");
     if (el) {
-      el.innerHTML = JSON.stringify(args || {}, null, 2)
+      el.innerHTML = JSON.stringify(args || {}, null, 2);
     }
   }
 
@@ -171,25 +171,25 @@ function App() {
         </div>
       </div>
 
-      <div id="console" style={{whiteSpace: 'pre-line'}}>
-        <p style={{whiteSpace: 'pre-line'}}></p>
+      <div id="console" style={{ whiteSpace: "pre-line" }}>
+        <p style={{ whiteSpace: "pre-line" }}></p>
       </div>
     </>
-  )
+  );
 
   const logoutView = (
     <button onClick={login} className="card">
       Login
     </button>
-  )
+  );
 
   return (
     <div className="container">
       <h1 className="title">
         <a target="_blank" href="http://web3auth.io/" rel="noreferrer">
           Web3Auth
-        </a>{' '}
-        Core & ReactJS Example for Google Login
+        </a>{" "}
+        & ReactJS Example for Google Login
       </h1>
 
       <div className="grid">{provider ? loginView : logoutView}</div>
@@ -211,7 +211,7 @@ function App() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
